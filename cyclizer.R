3c40f3acecda8e3627ea9fe3253bdad3d8c10b64
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-13 19:54:10 -0700

    Update cyclizer.R

--- cyclizer.R
+++ cyclizer.R
@@ -3,13 +3,25 @@
 #New cycle is scanned for atoms with too many bonds (particularly the newly-formed cycle). Cycle is destroyed if octet rule is 
 #not respected.
 #
-#
-cyclizer<-function(SMILES,n.ring,){
+#SMILES is the smiles string that will be cyclized (or bond formed)
+#steps is the number of steps (bond formations)
+cyclizer<-function(SMILES,steps,){
   SMILES<-as.character(SMILES)
-  n.ring<-as.numeric(n.ring)
+  steps<-as.numeric(steps)
+  list1<-list(list())
+#1. Cyclizer - (1) Program will find largest number in SMILES string and will (2) insert the next largest number to the right of 
+#each combination of two non-adjacent elements.
+cycles<-
+#2. sb to db - (1) Program will find all single bonds between two (adjacent) elements. (2) Program will count number of bonds 
+#around each element. (3) For elements which can carry another bond, the single connector between the two elements will 
+#become a "=".
+sb2db<-
+#3. db to tb - (1) For all "="
+db2tb<-
+#4. tb to qb
+tb2qb<-
 #
 #Insert looping function for inserting numbers to form new rings
-
 #Insert command for removing 
   
 }  
