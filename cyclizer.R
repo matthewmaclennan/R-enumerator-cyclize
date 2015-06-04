48f89b1375ab3b4fa2fee4edc21d4c719d8c5b31
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-04 14:10:41 -0700

    Update cyclizer.R

--- cyclizer.R
+++ cyclizer.R
@@ -5,6 +5,7 @@
 #All results stored in a list object
 #SMILES is the smiles string that will be cyclized (or bond-formed)
 #steps is the number of steps (bond formations)
+elementlist<-"H|He|Li|Be|B|C|N|O|F|Ne|Na|Mg|Al|Si|P|S|Cl|Ar|K|Ca|Sc|Ti|V|Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|Rb|Sr|Y|Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I|Xe|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W|Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|Fr|Ra|Ac|Th|Pa|U|Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Db|Sg|Bh|Hs|Mt|Ds|Rg|Cn|Uut|Uuq|Uup|Uuh|Uus|Uuo"
 cyclizer<-function(SMILES,steps,){
   SMILES<-as.character(SMILES)
   steps<-as.numeric(steps)
@@ -20,7 +21,8 @@ cyclizer<-function(SMILES,steps,){
 #2. sb to db - (1) Program will find all single bonds between two (adjacent) elements. (2) Program will count number of bonds 
 #around each element. (3) For elements which can carry another bond, the single connector between the two elements will 
 #become a "=".
-  singleBondFinder<-
+  singleBondFinderAdjacent<-"[A-Z]{2}|([A-Z][a-z]){2}|[A-Z]([A-Z][a-z])|([A-Z][a-z])[A-Z]"
+  singleBondFinderNumber<-
   boundCounter<-
   sb2db<-
 #3. db to tb - (1) Program will find all "=" in molecule. (2) Program will change, one-by-one, all "=" into "#".
