d0005475e3460d76ef770128fb53a4c8020720b9
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-04 12:12:05 -0700

    Update cyclizer.R

--- cyclizer.R
+++ cyclizer.R
@@ -10,20 +10,25 @@ cyclizer<-function(SMILES,steps,){
   steps<-as.numeric(steps)
   list1<-list(list())
 #1. Cyclizer - (1) Program will find largest number in SMILES string and will (2) insert the next largest number to the right of 
-#each combination of two non-adjacent elements.
-cycles<-
+#each combination of two non-adjacent elements. 
+#maxnum.l finds the maximum number in a smiles string (factoring in the % symbols). Returns a list of maximum numbers
+  maxnum.l<-lapply(regmatches(smiles,gregexpr("(?<=%)[0-9]{2,}|[0-9]{1}",smiles,perl=T)),function(x) max(as.numeric(x)))
+#If the numeric is greater than or equal to 10, paste0 "%" to the left of it. Otherwise, return x+1. This gives the new
+#text string for the new maximum number to be inserted which will form the new linkage.
+  newmax.l<-lapply(maxnum,function(x) if(x+1>=10) {paste0("%",x+1)} else {x+1})
+  cycles<-
 #2. sb to db - (1) Program will find all single bonds between two (adjacent) elements. (2) Program will count number of bonds 
 #around each element. (3) For elements which can carry another bond, the single connector between the two elements will 
 #become a "=".
-singleBondFinder<-
-boundCounter<-
-sb2db<-
+  singleBondFinder<-
+  boundCounter<-
+  sb2db<-
 #3. db to tb - (1) Program will find all "=" in molecule. (2) Program will change, one-by-one, all "=" into "#".
-db2tb<-
+  db2tb<-
 #4. tb to qb
-tb2qb<-
+  tb2qb<-
 #
 #Insert looping function for inserting numbers to form new rings
 #Insert command for removing 
-list1<-list1
+  list1<-list1
 }  
