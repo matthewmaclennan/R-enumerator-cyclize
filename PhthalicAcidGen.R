98868131e06906ff3c03cb412daf9a6d40e9b007
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-23 13:17:58 -0700

    Update PhthalicAcidGen.R

--- PhthalicAcidGen.R
+++ PhthalicAcidGen.R
@@ -7,8 +7,8 @@ require(ChemmineR)
 PA<-"c1c2c3ccc1.C2(=O)O.C3(=O)O"
 PAready<-"c1c2c3ccc1.C2(=O)4.C3(=O)5."
 alcoholSMILESmatrix<-as.matrix(unlist(strsplit(alcoholSMILES,"\n")))
-Onum4<-gsub("O","O4",expand.grid(alcoholSMILESmatrix,alcoholSMILESmatrix)[,1])
-Onum5<-gsub("O","O5",expand.grid(alcoholSMILESmatrix,alcoholSMILESmatrix)[,2])
+Onum4<-gsub("O","O4",alcoholSMILESmatrix)
+Onum5<-gsub("O","O5",alcoholSMILESmatrix)
 alcoholPrep<-apply(cbind(Onum4,Onum5),1,function(x) paste0(x,collapse="."))
 PA.mat<-paste0(PAready,alcoholPrep)
 PA.smiles<-paste0(PA.mat,collapse="\n")
