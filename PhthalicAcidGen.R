b76fd95f730b9412c987be88cd134b0262b0b82e
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-23 12:12:30 -0700

    Create PhthalicAcidGen.R

---
+++ PhthalicAcidGen.R
@@ -0,0 +1,14 @@
+#Generates phthalic acids from SMILES input. Phthalic acid base molecule is part-wise SMILES string (benzene and two COOH)
+#Phthalic acid OH are lopped off and replaced by alcohol.
+#
+#
+PhthalicAcidGen<-function(alcoholSMILES){
+PA<-"c1c2c3ccc1.C2(=O)O.C3(=O)O"
+PAready<-"c1c2c3ccc1.C2(=O)4.C3(=O)5"
+alcoholSMILESmatrix<-as.matrix(unlist(strsplit(alcoholSMILES,"\n")))
+Onum4<-gsub("O","O4",expand.grid(alcoholSMILESmatrix,alcoholSMILESmatrix)[,1])
+Onum5<-gsub("O","O5",expand.grid(alcoholSMILESmatrix,alcoholSMILESmatrix)[,2])
+alcoholPrep<-apply(cbind(Onum4,Onum5),1,function(x) paste0(x,collapse="."))
+AlcoholPrepNum<-alcoholPrep
+
+}
